e2619d2d3eec68433a869bcad6d05e21325bac19
Chris <cshoeman02@gmail.com>
2018-07-04 18:32:30 -0600

    Inventory and item support
    Inventory and items are available now. There is a sandwich in the test kitchen room that you can take using the command 'take Sandwich' and drop it using the command 'drop Sandwich' once it is in your inventory.

--- PyRPG.py
+++ PyRPG.py
@@ -1,10 +1,11 @@
 # this is a test
-hp, mhp = 10, 10
+hp, mhp, lvl, xp, mxp = 10, 10, 1, 0, 100
+gld = 0
 x, y, z = 0, 0, 0
 
 def main():
 	define_rooms()
-	print("\nPyRPG\nby Chris Shoeman (morkimer).\nhttps://github.com/morkimer/PyRPG\nThis is my first Python project, and I am learning Python on the fly as I write the code. If there is anything I can do to improve the code, feel free to submit an issue on the Issues page on Github or submit a pull request.\n\nYour health is ",hp,"/",mhp,".\n")
+	print("\nPyRPG\nby Chris Shoeman (morkimer).\nhttps://github.com/morkimer/PyRPG\nThis is my first Python project, and I am learning Python on the fly as I write the code. If there is anything I can do to improve the code, feel free to submit an issue on the Issues page on Github or submit a pull request.\n\nYour health is ",hp,"/",mhp," and you have",inventory,"in your inventory.\n")
 	while True:
 		getRoom()
 		get_input()
@@ -15,8 +16,48 @@ def get_input():
 		move(cmd)
 	elif cmd == "exit" or cmd == "quit":
 		exit()
+	elif cmd == "inventory" or cmd == "inv":
+		if inventory != []:
+			itemlist = "Items in inventory: "
+			itemstring = ""
+			for a in inventory:
+				if itemstring == "":
+					itemstring = str(a.name)
+				else:
+					itemstring = itemstring,str(a.name)
+			print(itemlist + str(itemstring) + "\n")
+		else:
+			print("You have nothing in your inventory.\n")
 	else:
-		print("Unknown command.")
+		if len(cmd.split()) > 0:
+			cmdwords = cmd.split()
+			if cmdwords[0] == "say":
+				saystring = " ".join(cmdwords[1:])
+				print('You said "' + saystring + '".')
+			elif cmdwords[0] == "take":
+				for r in rooms:
+					if r.cx == x and r.cy == y and r.cz == z:
+						print(r.name," ("+ str(x) +","+ str(y) +","+ str(z) +")\n",r.desc,"\n")
+						if r.items != []:
+							for a in r.items:
+								if cmdwords[1] == a.name:
+									inventory.append(a)
+									r.items.remove(a)
+									print("You took " + str(a.name) + ".")
+			elif cmdwords[0] == "drop":
+				for r in rooms:
+					if r.cx == x and r.cy == y and r.cz == z:
+						print(r.name," ("+ str(x) +","+ str(y) +","+ str(z) +")\n",r.desc,"\n")
+						if inventory != []:
+							for a in inventory:
+								if cmdwords[1] == a.name:
+									r.items.append(a)
+									inventory.remove(a)
+									print("You dropped " + str(a.name) + ".")
+			else:
+				print("Unknown command.")
+		else:
+			print("Unknown command.")
 
 def move(direction):
 	global x
@@ -65,6 +106,15 @@ def getRoom():
 	for r in rooms:
 		if r.cx == x and r.cy == y and r.cz == z:
 			print(r.name," ("+ str(x) +","+ str(y) +","+ str(z) +")\n",r.desc,"\n")
+			if r.items != []:
+				itemlist = "Items in room: "
+				itemstring = ""
+				for a in r.items:
+					if itemstring == "":
+						itemstring = str(a.name)
+					else:
+						itemstring = itemstring,str(a.name)
+				print(itemlist + str(itemstring) + "\n")
 
 class room:
 	def __init__(self, name, desc, cx, cy, cz, exitN, exitW, exitS, exitE, exitU, exitD, items):
@@ -80,13 +130,28 @@ class room:
 		self.exitU = exitU
 		self.exitD = exitD
 		self.items = items
+class item:
+	def __init__(self, name, desc, level, itype, attributes):
+		self.name = name
+		self.desc = desc
+		self.level = level
+		self.itype = itype
+		self.attributes = attributes
+class attr:
+	def __init__(self, attribute, value):
+		self.attribute = attribute
+		self.value = value
 
 def define_rooms():
 	global rooms
 	rooms = [
 		room("Test Room", "Lorem ipsum dolor sit amet...", 0, 0, 0, False, True, False, False, False, True, []),
 		room("Test Basement", "This is the basement of the test room.", 0, -1, 0, False, False, False, False, True, False, []),
-		room("Test Hallway", "Wow! You can access rooms here.", -1, 0, 0, False, False, False, True, False, False, [])
+		room("Test Hallway", "Wow! You can access rooms here.", -1, 0, 0, True, True, True, True, True, False, []),
+		room("Test Living Room", "There is stuff here.", -2, 0, 0, True, False, False, True, False, False, []),
+		room("Test Kitchen", "Food was prepared here.", -2, 0, 1, False, False, True, False, False, False, [item("Sandwich", "Restores 5 HP", 0, "food", [attr("heal",5)])]),
+		room("Test Bathroom", "Stuff.", -1, 0, 1, False, False, True, False, False, False, []),
+		room("Test Closet", "Ben is a ho", -1, 0, -1, True, False, False, False, False, False, [])
 		]
 
 
@@ -94,17 +159,6 @@ def define_rooms():
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 # do not move this, keep at bottom
+inventory = []
 main()
